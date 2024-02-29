007a0f86ea42c8a2cf48e71a10eec35dfa28797f
balen nouri <balennouri@gmail.com>
2024-02-29 17:14:06 +0000

    Feat. Add secret key

--- fastshoe/settings.py
+++ fastshoe/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wljxbfm_ovay225h(9$@oms0ol6jf4$_xmepky2l893@lwxv+b'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
