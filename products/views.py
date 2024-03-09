688ffffc3b72ffa1ffe87502ad913d9c2423b0c0
balen nouri <balennouri@gmail.com>
2024-03-09 19:13:34 +0000

    Feat: Add products view

--- products/views.py
+++ products/views.py
@@ -73,10 +73,21 @@ def product_view(request, product_id):
 
 def add_product(request):
     """
-    Add product to the store(product management) on the
-    store page as a admin
+    Add a product to the store thorug the product management
     """
-    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(
+                request,
+                'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+ 
     template = 'products/add_product.html'
     context = {
         'form': form,
