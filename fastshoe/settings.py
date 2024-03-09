9a1980b2b35a38b04948700eed6cda2fb460c42f
balen nouri <balennouri@gmail.com>
2024-03-09 09:01:40 +0000

    Fix: Add new databse

--- fastshoe/settings.py
+++ fastshoe/settings.py
@@ -136,6 +136,12 @@ WSGI_APPLICATION = "fastshoe.wsgi.application"
 
 DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
 
 CSRF_TRUSTED_ORIGINS = [
     "http://*",
