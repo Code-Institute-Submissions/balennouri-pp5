59b5b23ca3df4a5027b1c343edfc1d971a507383
balen nouri <balennouri@gmail.com>
2024-03-11 23:44:58 +0000

    Feat: Fix codes with flake 8

--- bag/views.py
+++ bag/views.py
@@ -59,7 +59,8 @@ def adjust_bag(request, item_id):
         if quantity > 0:
             bag[item_id]["items_by_size"][size] = quantity
             messages.success(request, f'Updated size {size.upper()} \
-                {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
+                {product.name} \
+                    quantity to {bag[item_id]["items_by_size"][size]}')
         else:
             del bag[item_id]["items_by_size"][size]
             if not bag[item_id]["items_by_size"]:
