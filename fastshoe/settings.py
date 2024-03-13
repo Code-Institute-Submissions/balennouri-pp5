56c5a1a9a1ad7575185c3c10199a79d6cc943103
balen nouri <balennouri@gmail.com>
2024-03-13 18:36:51 +0000

    Fix: debug fales

--- fastshoe/settings.py
+++ fastshoe/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "https://8000-balennouri-pp5-5oy58itgibp.ws-eu108.gitpod.io",
