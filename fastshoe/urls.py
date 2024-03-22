0d5a3fcea6c5127266b62578a395ee6e763322a6
balen nouri <balennouri@gmail.com>
2024-03-22 19:58:03 +0000

    Feat: Add Url for robots.txt

--- fastshoe/urls.py
+++ fastshoe/urls.py
@@ -1,25 +1,9 @@
-"""
-URL configuration for fastshoe project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import handler404
+from django.views.generic import TemplateView
 
 handler404 = 'fastshoe.views.handler404'
 
@@ -31,4 +15,14 @@ urlpatterns = [
     path("bag/", include("bag.urls")),
     path("checkout/", include("checkout.urls")),
     path('profile/', include('profiles.urls')),
+
+
+    # Had to use https://adamj.eu/tech/2020/02/10/robots-txt/ to
+    # get my robots.txt readable by lighthouse
+    path(
+        "robots.txt",
+        TemplateView.as_view(
+            template_name="robots.txt", content_type="text/plain")
+        ),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
